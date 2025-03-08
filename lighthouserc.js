c22aa8d3ccc71c944fd7af15fd2d461184276b29
pia <sysadmin@pia-corp.jp>
2025-03-08 15:52:41 +0900

    taergad

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,14 +2,15 @@ module.exports = {
   ci: {
     collect: {
       // 静的ビルドされたアプリケーションをテストするための設定
-      staticDistDir: './public', // あなたのビルドディレクトリに合わせて変更してください
+      staticDistDir: './build', // あなたのビルドディレクトリに合わせて変更してください
       // または動的サーバーをテストするためのURL設定
       // startServerCommand: 'npm run start', // 開発サーバーを起動するコマンド
       // url: ['http://localhost:3000', 'http://localhost:3000/about'], // テストするURL
       numberOfRuns: 1, // 各ページで実行するLighthouseの回数
     },
     upload: {
-      target: 'temporary-public-storage', // 結果の保存先（GitHub上でもアクセス可能）
+      target: 'filesystem', // ローカルファイルシステムに保存
+      outputDir: '.lighthouseci', // 出力ディレクトリ
     },
     assert: {
       // アサーション設定 - これらはオプションです
