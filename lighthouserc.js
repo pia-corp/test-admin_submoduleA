9b6f00fd18aa6ac6ede1f7048ff1630df6ed3858
pia <sysadmin@pia-corp.jp>
2025-03-08 16:20:41 +0900

    avav

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,27 +1,13 @@
 module.exports = {
   ci: {
     collect: {
-      // 静的ビルドされたアプリケーションをテストするための設定
+      url: ['http://localhost:3000/'],
       staticDistDir: './public', // あなたのビルドディレクトリに合わせて変更してください
-      // または動的サーバーをテストするためのURL設定
-      // startServerCommand: 'npm run start', // 開発サーバーを起動するコマンド
-      // url: ['http://localhost:3000', 'http://localhost:3000/about'], // テストするURL
+      startServerCommand: 'rails server -e production',
       numberOfRuns: 1, // 各ページで実行するLighthouseの回数
     },
     upload: {
-      target: 'filesystem', // ローカルファイルシステムに保存
-      outputDir: '.lighthouseci', // 出力ディレクトリ
+      target: 'temporary-public-storage',
     },
-    // assert: {
-    //   // アサーション設定 - これらはオプションです
-    //   // 必要に応じてパフォーマンス閾値を設定できます
-    //   preset: 'lighthouse:recommended',
-    //   assertions: {
-    //     'categories:performance': ['warn', {minScore: 0.6}],
-    //     'categories:accessibility': ['warn', {minScore: 0.8}],
-    //     'categories:best-practices': ['warn', {minScore: 0.8}],
-    //     'categories:seo': ['warn', {minScore: 0.8}],
-    //   },
-    // },
   },
 };
