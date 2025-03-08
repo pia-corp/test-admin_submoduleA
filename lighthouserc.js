6d4ff48736d15ba42f2b51b40c93397fb4a8f55b
pia <sysadmin@pia-corp.jp>
2025-03-08 16:28:57 +0900

    ava

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,6 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:3000/'],
       staticDistDir: './public', // あなたのビルドディレクトリに合わせて変更してください
       startServerCommand: 'rails server -e production',
       numberOfRuns: 1, // 各ページで実行するLighthouseの回数
