ddae5c65174c857dbc316e0aaebbbe59562744eb
pia <sysadmin@pia-corp.jp>
2025-03-10 15:59:19 +0900

    gaga

--- link-checker.js
+++ link-checker.js
@@ -1,89 +1,89 @@
-const { SiteChecker } = require("broken-link-checker");
-const fs = require('fs');
-const path = require('path');
-const cheerio = require('cheerio');
+// const { SiteChecker } = require("broken-link-checker");
+// const fs = require('fs');
+// const path = require('path');
+// const cheerio = require('cheerio');
 
-const publicDir = path.join(__dirname, 'public/');
+// const publicDir = path.join(__dirname, 'public/');
 
-function getHtmlFiles(dir) {
-  let htmlFiles = [];
-  const files = fs.readdirSync(dir);
-  files.forEach(file => {
-    const filePath = path.join(dir, file);
-    const stat = fs.statSync(filePath);
-    if (stat.isDirectory()) {
-      htmlFiles = htmlFiles.concat(getHtmlFiles(filePath));
-    } else if (file.endsWith('.html')) {
-      htmlFiles.push(filePath);
-    }
-  });
-  return htmlFiles;
-}
+// function getHtmlFiles(dir) {
+//   let htmlFiles = [];
+//   const files = fs.readdirSync(dir);
+//   files.forEach(file => {
+//     const filePath = path.join(dir, file);
+//     const stat = fs.statSync(filePath);
+//     if (stat.isDirectory()) {
+//       htmlFiles = htmlFiles.concat(getHtmlFiles(filePath));
+//     } else if (file.endsWith('.html')) {
+//       htmlFiles.push(filePath);
+//     }
+//   });
+//   return htmlFiles;
+// }
 
-const htmlFiles = getHtmlFiles(publicDir);
+// const htmlFiles = getHtmlFiles(publicDir);
 
-const brokenLinks = {};
-const checkedFiles = [];
+// const brokenLinks = {};
+// const checkedFiles = [];
 
-function removeDuplicateLinks(brokenLinks) {
-  for (const file in brokenLinks) {
-    if (brokenLinks.hasOwnProperty(file)) {
-      brokenLinks[file] = [...new Set(brokenLinks[file])];
-    }
-  }
-}
+// function removeDuplicateLinks(brokenLinks) {
+//   for (const file in brokenLinks) {
+//     if (brokenLinks.hasOwnProperty(file)) {
+//       brokenLinks[file] = [...new Set(brokenLinks[file])];
+//     }
+//   }
+// }
 
-const siteChecker = new SiteChecker({
-  excludedKeywords: ['https://fonts.googleapis.com', 'https://fonts.gstatic.com'],
-  excludeExternalLinks: false,
-  excludeInternalLinks: false,
-  excludeLinksToSamePage: true,
-  filterLevel: 3,
-  acceptedSchemes: ["http", "https"],
-  requestMethod: "get"
-}, {
-  link: (result) => {
-    if (result.broken) {
-      // 正規表現を使用してプロトコル + ドメイン部分を削除
-      const file = result.base.original.replace(/^https?:\/\/[^/]+/, '');
+// const siteChecker = new SiteChecker({
+//   excludedKeywords: ['https://fonts.googleapis.com', 'https://fonts.gstatic.com'],
+//   excludeExternalLinks: false,
+//   excludeInternalLinks: false,
+//   excludeLinksToSamePage: true,
+//   filterLevel: 3,
+//   acceptedSchemes: ["http", "https"],
+//   requestMethod: "get"
+// }, {
+//   link: (result) => {
+//     if (result.broken) {
+//       // 正規表現を使用してプロトコル + ドメイン部分を削除
+//       const file = result.base.original.replace(/^https?:\/\/[^/]+/, '');
 
-      if (file != "/") {
-        if (!brokenLinks[file]) {
-          brokenLinks[file] = [];
-        }
-        brokenLinks[file].push(result.url.original);
-      }
+//       if (file != "/") {
+//         if (!brokenLinks[file]) {
+//           brokenLinks[file] = [];
+//         }
+//         brokenLinks[file].push(result.url.original);
+//       }
 
-    } else {
-      // console.log(`${result.url.original}: Valid`);
-    }
-  },
-  end: async () => {
-    console.log("Link checking completed.");
-    removeDuplicateLinks(brokenLinks); // 重複を削除
-    await notifyGitHub(brokenLinks);
-    console.log("Checked files:");
-    checkedFiles.forEach(file => console.log(file));
-  }
-});
+//     } else {
+//       // console.log(`${result.url.original}: Valid`);
+//     }
+//   },
+//   end: async () => {
+//     console.log("Link checking completed.");
+//     removeDuplicateLinks(brokenLinks); // 重複を削除
+//     await notifyGitHub(brokenLinks);
+//     console.log("Checked files:");
+//     checkedFiles.forEach(file => console.log(file));
+//   }
+// });
 
-htmlFiles.forEach(filePath => {
-  const content = fs.readFileSync(filePath, 'utf8');
-  const $ = cheerio.load(content);
+// htmlFiles.forEach(filePath => {
+//   const content = fs.readFileSync(filePath, 'utf8');
+//   const $ = cheerio.load(content);
 
-  $('a[target="_blank"]').each((index, element) => {
-    $(element).attr('href', '#');
-  });
+//   $('a[target="_blank"]').each((index, element) => {
+//     $(element).attr('href', '#');
+//   });
 
-  fs.writeFileSync(filePath, $.html());
-  siteChecker.enqueue(`http://localhost:8081/${path.relative(publicDir, filePath)}`);
-});
+//   fs.writeFileSync(filePath, $.html());
+//   siteChecker.enqueue(`http://localhost:8081/${path.relative(publicDir, filePath)}`);
+// });
 
-async function notifyGitHub(brokenLinks) {
-  const outputPath = process.env.GITHUB_OUTPUT;
-  if (outputPath) {
-    const errors = JSON.stringify(brokenLinks);
-    fs.appendFileSync(outputPath, `errors=${errors}\n`);
-  }
-  console.log(`GitHub Notice: Broken links detected - ${JSON.stringify(brokenLinks)}`);
-}
+// async function notifyGitHub(brokenLinks) {
+//   const outputPath = process.env.GITHUB_OUTPUT;
+//   if (outputPath) {
+//     const errors = JSON.stringify(brokenLinks);
+//     fs.appendFileSync(outputPath, `errors=${errors}\n`);
+//   }
+//   console.log(`GitHub Notice: Broken links detected - ${JSON.stringify(brokenLinks)}`);
+// }
