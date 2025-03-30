8e72b3aca57025fb228435ffeeb7a72f24018521
pia <sysadmin@pia-corp.jp>
2025-03-30 22:53:33 +0900

    asdag

--- .github/actions/link-checker.js
+++ .github/actions/link-checker.js
@@ -5,7 +5,7 @@ const cheerio = require('cheerio');
 
 const REPOSITORY = process.env.REPOSITORY;
 console.log(__dirname);
-const publicDir = path.join('/home/runner/work', REPOSITORY, 'public/');
+const publicDir = path.join('/home/runner/work', REPOSITORY, REPOSITORY, 'public/');
 console.log(publicDir);
 // const publicDir = path.join(__dirname, 'public/');
 
